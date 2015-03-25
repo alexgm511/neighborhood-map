4e7a5034a4d3c62a36397867ebf5d0cdd274f349
Alex G Mendoza <alexgm511@gmail.com>
2015-03-25 10:31:00 -0700

    Incorporated Google services into map and linked list of services to map markers, added types of services to markers

--- js/app.js
+++ js/app.js
@@ -2,48 +2,53 @@ function WebmailViewModel() {
     // Data
     var self = this;
 	// Google map variables
-	//self.map;
 	self.geocoder;
 	self.service;
 	self.location;
 	self.infowindow;
+	self.services = ko.observableArray([]);
+	self.showServices = ko.observable(false);
+	self.hideMarkers = function(){
+		self.clearMarkers();
+	};
+	self.showMarkers = function(){
+		self.setAllMap(self.map);
+	};
+	self.markers = [];
 
 	// Input field for neighborhood
     self.neigborhood = ko.observable();
 	self.map = ko.observable();
 	
-	// Google map api function initialized with Washington, DC coordinates
+	// Initialize Google Map API. 
+	// Initialized with Washington, DC coordinates
 	// so that we won't have a blank map on start
 	self.initialize = function() {
 		self.geocoder = new google.maps.Geocoder();
 		var mapOptions = {
 			//coordinates to put the white house in the center
-			center: { lat: 38.897192, lng: -77.036871 },
+			center: { lat: 38.899192, lng: -77.036871 },
 			zoom: 16
 		};
 		self.map = new google.maps.Map(document.getElementById('map-canvas'),
 		mapOptions);
+		// Get the location object from the current map
+		var myLocation = self.map.getCenter();
+		self.addServices(myLocation, 'store');
 	};
+	
 	// Google Map function that does the address to latitude/longitude lookup.
 	self.codeAddress = function() {
 	  var address = self.neigborhood();
 	  self.geocoder.geocode( { 'address': address}, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
-			self.map.setCenter(results[0].geometry.location);
 			self.location = results[0].geometry.location;
-			console.log(results[0].geometry.location);
+			self.map.setCenter(self.location);
 			var marker = new google.maps.Marker({
 			  map: self.map,
-			  position: results[0].geometry.location
+			  position: self.location
 			});
-			var request = {
-			  location: results[0].geometry.location,
-			  radius: '500',
-			  types: ['store']
-			};
-			self.infowindow = new google.maps.InfoWindow();
-			self.service = new google.maps.places.PlacesService(self.map);
-			self.service.nearbySearch(request, self.svsCallback)
+			self.addServices(self.location, 'store');
 		} else {
 			alert('Sorry, could not find your neighborhood for the following reason: ' + status);
 		}
@@ -55,28 +60,75 @@ function WebmailViewModel() {
 		self.codeAddress();
 	};
 	
+	// Call Google's PlacesService option to get local businesses 
+	self.addServices = function(myLocation, type) {
+		var request = {
+			location: myLocation,
+			radius: '500',
+			types: [type]
+		};
+		self.infowindow = new google.maps.InfoWindow();
+		self.service = new google.maps.places.PlacesService(self.map);
+		self.service.nearbySearch(request, self.svsCallback)
+	}
+	
+	// PlacesService callback function
 	self.svsCallback = function(results, status) {
 		if (status == google.maps.places.PlacesServiceStatus.OK) {
 			for (var i = 0; i < results.length; i++) {
 				console.log(results[i]);
-			  var place = results[i];
-			  self.createMarker(results[i]);
+				self.services.push(results[i]);
+				var place = results[i];
+				self.createMarker(place);
 			}
 		  }
 	}
-
+	
+	// Map marker generator
 	self.createMarker = function(place) {
 		var placeLoc = place.geometry.location;
 		var marker = new google.maps.Marker({
-			map: self.map,
-			position: place.geometry.location
+			map: null,  //self.map,
+			//position: place.geometry.location
+			position: placeLoc
 		});
+		self.markers.push(marker);
+		var types = place.types;
+		var markerTxt = types.join('<br />');
 
 		google.maps.event.addListener(marker, 'click', function() {
-			console.log(place.name);
-			self.infowindow.setContent(place.name);
+			self.infowindow.setContent('<strong>'+place.name+'</strong>'+'<br />'+markerTxt);
 			self.infowindow.open(self.map, this);
 		});
+	}
+	
+	// Sets the map on all markers in the array.
+	self.setAllMap = function(map) {
+	  for (var i = 0; i < self.markers.length; i++) {
+		self.markers[i].setMap(map);
+	  }
+	};
+
+		// Sets the map on all markers in the array.
+	self.showPlace = function() {
+		var location = this.geometry.location;
+	  for (var i = 0; i < self.markers.length; i++) {
+		  if (self.markers[i].position == location) {
+				self.markers[i].setMap(self.map);
+				self.map.panTo(location);
+				google.maps.event.trigger(self.markers[i], 'click');
+				//self.markers[i].click();
+			}	
+	  }
+		window.setTimeout(function() {
+			self.map.setCenter(location);
+		}, 3000);
+		//trigger(instance:Object, eventName:string, var_args:*)
+	};
+
+	// Removes the markers from the map, but keeps them in the array.
+	self.clearMarkers = function() {
+		self.setAllMap(null);
 	}
 	
 	// Event listens for page load to initialize Google maps API
